a70a98254e466912d1855a5a6342d32b9e3f4a79
I-migi <sunnn10189@gmail.com>
2024-09-12 21:53:06 +0900

    spring interceptor

--- src/main/java/hello/login/web/HomeController.java
+++ src/main/java/hello/login/web/HomeController.java
@@ -7,12 +7,8 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.SessionAttribute;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
 
 @Slf4j
 @Controller
@@ -85,5 +81,6 @@ public class HomeController {
         model.addAttribute("member", loginMember);
         return "loginHome";
     }
+
 
 }
